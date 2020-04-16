77c932d8c1d4c5dee5fd567afc9e9e55743cd6fd
Christoffer home <christoffer.kant@student.hu.nl>
2020-04-16 22:33:48 +0200

    Inhoud not working yet

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] arg) {
-        Zwembad z1 = new Zwembad("2.0", "5.5", "1.5");
-        System.out.println("LENGTE: " + z1.getLengte());
+        Zwembad z1 = new Zwembad(2.0, 5.5, 1.5);
         System.out.println("BREEDTE: " + z1.getBreedte());
+        System.out.println("LENGTE: " + z1.getLengte());
         System.out.println("DIEPTE: " + z1.getDiepte());
+        System.out.println("BEREKENDE INHOUD: " + getInhoud());
+        System.out.println();
+
+        Zwembad z2 = new Zwembad(
+                2.5,
+                100.0,
+                2.0);
+        System.out.println(z2.toString());
+        System.out.println("BEREKENDE INHOUD: " + inhoud());
     }
 }
--- src/Zwembad.java
+++ src/Zwembad.java
@@ -1,27 +1,30 @@
 public class Zwembad {
-    private String lengte;
-    private String breedte;
-    private String diepte;
+    private double breedte;
+    private double lengte;
+    private double diepte;
 
-    public Zwembad(String lng, String br, String di)
+    public Zwembad(double br, double lng, double di)
     {
-        lengte = lng;
         breedte = br;
+        lengte = lng;
         diepte = di;
     }
 
-    public String getLengte() {
-        return lengte;
-    }
-
-    public String getBreedte() {
+    public double getBreedte() {
         return breedte;
     }
 
-    public String getDiepte() {
+    public double getLengte() {
+        return lengte;
+    }
+
+    public double getDiepte() {
         return diepte;
     }
 
+    public double getInhoud() {
+        return breedte * lengte * diepte;
+    }
     public String toString() {
         String s = "Dit zwembad is " + breedte + " meter breed, " + lengte + " meter lang, en " + diepte + " meter diep";
         return s;
