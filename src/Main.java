53c58be6df1b8c57eb2c44ed6e1796366caf5325
Christoffer home <christoffer.kant@student.hu.nl>
2020-04-16 14:18:16 +0200

    Lengte breedte en diepte van z1 print

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] arg) {
+        Zwembad z1 = new Zwembad("2.0", "5.5", "1.5");
+        System.out.println("LENGTE: " + z1.getLengte());
+        System.out.println("BREEDTE: " + z1.getBreedte());
+        System.out.println("DIEPTE: " + z1.getDiepte());
+    }
 }
--- src/Zwembad.java
+++ src/Zwembad.java
@@ -1,5 +1,29 @@
 public class Zwembad {
     private String lengte;
     private String breedte;
-    
+    private String diepte;
+
+    public Zwembad(String lng, String br, String di)
+    {
+        lengte = lng;
+        breedte = br;
+        diepte = di;
+    }
+
+    public String getLengte() {
+        return lengte;
+    }
+
+    public String getBreedte() {
+        return breedte;
+    }
+
+    public String getDiepte() {
+        return diepte;
+    }
+
+    public String toString() {
+        String s = "Dit zwembad is " + breedte + " meter breed, " + lengte + " meter lang, en " + diepte + " meter diep";
+        return s;
+    }
 }
